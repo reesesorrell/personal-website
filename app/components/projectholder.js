269e787a6667dc03eeb24a2bc09d1dee2d22efb9
Reese Sorrell <joreesesorrell@gmail.com>
2023-08-24 18:50:23 -0500

    Add grid to project screen

--- app/components/projectholder.js
+++ app/components/projectholder.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function ProjectHolder({picUrl, description, url, code, inverted}) {
     return (
-        <div className='text-white font-serif flex flex-col'>
+        <div className='text-white font-serif flex flex-col row-span-2 col-span-2 justify-self-center  w-3/5'>
             <Image src={picUrl}
                     width={500}
                     height={200}
