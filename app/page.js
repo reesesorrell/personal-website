2c20d6fac54507bff954d98e5b380a4a5221216e
Reese Sorrell <joreesesorrell@gmail.com>
2023-08-23 21:59:12 -0500

    Add paging template

--- app/page.js
+++ app/page.js
@@ -4,8 +4,14 @@ export default function Home() {
       <div  className='background h-screen w-screen flex items-center justify-center'>
         <div className='first-name text-white text-4xl font-serif'>Reese Sorrell</div>
       </div>
-      <div  className='bg-white h-screen w-screen flex items-center justify-center'>
-        <div className='first-name text-black text-4xl font-serif'>Reese Sorrell</div>
+      <div  className='bg-slate-900 h-screen-double w-screen flex items-center justify-center'>
+        <div className='first-name text-white text-4xl font-serif'>About</div>
+      </div>
+      <div  className='background h-screen-double w-screen flex items-center justify-center'>
+        <div className='first-name text-white text-4xl font-serif'>Projects</div>
+      </div>
+      <div  className='bg-slate-900 h-screen w-screen flex items-center justify-center'>
+        <div className='first-name text-white text-4xl font-serif'>Contact</div>
       </div>
     </div>
   )
