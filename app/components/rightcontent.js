92d8eee103038b2a791444fdf245a441edae44e5
Reese Sorrell <joreesesorrell@gmail.com>
2023-08-25 14:27:26 -0500

    Add cursor radius and chonc hover

--- app/components/rightcontent.js
+++ app/components/rightcontent.js
@@ -2,7 +2,7 @@
 
 export default function() {
     return(
-        <div className="right-side mr-32 pt-24">
+        <div className="right-side pr-32 pt-24">
             <div className="about-me grey-color">
                 <div className="mb-6">
                     In 2021, I discovered my passion for the world of programming, coding, and technology as a whole. Every since then I haven't been able to stop learning, growing, and now pursuing my interests through college studies where I get to take cool classes like <em>Data Structures</em> and <em>Computer Organization</em>.
@@ -11,7 +11,7 @@ export default function() {
                     My main focus these days is succeeding in my classes while also doing <em>web development</em> and building cool sites like (hopefully) this one. I'm also in the process of <em>learning backend databases like MongoDB and SQL</em>.
                 </div>
                 <div className="mb-6">
-                    When I’m not at the computer, I’m usually swimming laps, lifting weight, hanging out with my amazing girlfried, or rolling it down in <em className="tft">Teamfight Tactics</em>.</div>
+                    When I’m not at the computer, I’m usually swimming laps, lifting weight, hanging out with my amazing girlfried, or rolling it down in <div className="tft inline"><em>Teamfight Tactics</em></div>.</div>
                 </div> 
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -2,8 +2,20 @@
 
 import LeftSideBar from "./components/leftsidebar"
 import RightContent from "./components/rightcontent"
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    const page = document.querySelector(".page");
+
+    page.addEventListener("mousemove", (e) => {
+    const { x, y } = page.getBoundingClientRect();
+    page.style.setProperty("--x", e.clientX - x);
+    page.style.setProperty("--y", e.clientY - y);
+  });
+  }, [])
+
   return (
     <div className="page flex justify-around">
       <LeftSideBar />
